5c92d85dd919a6df2a7074695ea5f7059d841e20
Austin King <shout@ozten.com>
2014-05-16 14:38:40 -0700

    A simple deployment script for github hooks

---
+++ simple_deploy.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+var exec = require('child_process').exec;
+var http = require('http');
+
+var config = require('./config');
+
+
+var gitPull = 'git pull origin master';
+var foreverRestart = 'forever restartall';
+
+var server = http.createServer(function(req, res) {
+  if (config.secret === req.url) {
+    res.write(gitPull);
+
+    var opts = {
+      cwd: config.deployDir
+    };
+    exec(gitPull, opts, function(err, stdout, stderr) {
+      if (stdout) res.write(stdout);
+      if (stderr) res.write(stderr);
+      if (err) {
+        res.write(err.toString());
+        res.statusCode = 500;
+        return res.end('Unable to do git pull');
+      }
+      res.write(foreverRestart);
+      exec(foreverRestart, opts, function(err, stdout, stderr) {
+        if (stdout) res.write(stdout);
+        if (stderr) res.write(stderr);
+        if (err) {
+          res.write(err.toString());
+          res.statusCode = 500;
+          return res.end('Unable to do forever restart');
+        }
+        res.statusCode = 200;
+        res.end('Finished');
+      });
+    });
+  } else {
+    console.log('Got _' + req.url + '_ wanted _' + config.secret + '_');
+    setTimeout(function() {
+      res.statusCode = 403;
+      res.end('Access Denied');
+    }, 3000);
+  }
+});
+
+server.listen(config.port, config.bindAddress, function() {
+  console.log('Listening for redeployments on port', config.port);
+});
